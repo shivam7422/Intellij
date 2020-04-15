54e6b71714c5d3a05125b43bb6dfe3abbdde4ccb
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2020-04-15 15:43:34 +0530

    Original code

---
+++ src/main/scala/Test/mos.scala
@@ -0,0 +1,18 @@
+package Test
+
+object mos extends  App{
+
+    val a=1
+    val b=1
+    println (" the sum of a and b is "+(a+b))
+
+    if (a>b)
+      println(" A is greater")
+    else if (a<b)
+      println(" B is greater ")
+    else
+    {
+      println(" the number is equal")
+    }
+
+  }
