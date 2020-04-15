95fb4c9f29038700607236ecaa45e9a784db847b
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2020-04-15 16:05:20 +0530

    Updated in Test

--- src/main/scala/Test/Test.scala
+++ src/main/scala/Test/Test.scala
@@ -1,18 +1,36 @@
 package Test
 
-object Test extends App{
-
-  val a=1
-  val b=1
-  println (" the sum of a and b is "+(a+b))
-
-  if (a>b)
-    println(" A is greater")
-  else if (a<b)
-    println(" B is greater ")
-  else
+class Student
+{
+  val art="shivam"
+}
+class Teacher extends Student
+{
+  def details: Unit =
   {
-    println(" the number is equal")
+    println(" My name is " + art)
+    println(" I am Teacher")
   }
+}
+class Exam extends Teacher
+{
+  def details1: Unit =
+  {
+    println(" Listening Songs ")
+  }
+}
+class door extends Exam
+{
+  def details2: Unit =
+  {
+    print(" this is door ")
+  }
+}
+
+object mos extends App{
+  val s= new door
+  s.details
+  s.details1
+  s.details2
 
 }
