4ecdf25227919055f4bbb7c4ae66bf41bfa1f673
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2020-04-15 11:41:46 +0530

    updated one

--- src/main/scala/Test/Test.scala
+++ src/main/scala/Test/Test.scala
@@ -5,9 +5,7 @@ object Test extends App{
   println(" This is me")
   println(" this is something")
   println(" this is you")
-
-  println(" this is something we need")
-  println(" this is what we can do ")
-  println(" this is bottle ")
   println(" this is table ")
+  println(" this is something new")
+
 }
