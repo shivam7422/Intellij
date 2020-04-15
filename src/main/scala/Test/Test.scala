15790d227f407a67d7e49b737483eac56068bfac
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2020-04-15 15:17:46 +0530

    Merged successfully

--- src/main/scala/Test/Test.scala
+++ src/main/scala/Test/Test.scala
@@ -2,9 +2,14 @@ package Test
 
 object Test extends App{
 
- val a=1
+  val a=1
   val b=2
-  print (" the sum of a and b is "+(a+b))
+  println (" the sum of a and b is "+(a+b))
+
+  if (a>b)
+    println(" A is greater")
+  else
+    println(" B is greater ")
 
 
 }
