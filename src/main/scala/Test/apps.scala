d92684f6a0d351b4a7159802beccf5b74f640b19
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2020-04-20 11:37:21 +0530

    New code updted in the new branch 	# Please enter the commit message for your changes. Lines starting

--- src/main/scala/Test/apps.scala
+++ src/main/scala/Test/apps.scala
@@ -1,6 +1,25 @@
 package Test
 
-class 
-object apps {
+class Ram
+{
+  def details: Unit =
+  {
+    val s= " shivam"
+    println(" the name which we are going to use is " + s)
+  }
+
+}
+class Mohan extends Ram
+{
+  def details2: Unit =
+  {
+    println(" this code is awesome ")
+  }
+}
+object apps  extends  App{
+
+  val z= new Mohan
+  z.details2
+  z.details
 
 }
